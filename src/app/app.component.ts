f335464af9a4f43b581cf1ecbd580f38baf5c2a1
kamgirgit <kamaran.jmi12@gmail.com>
2019-06-11 19:11:51 +0530

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { THROW_IF_NOT_FOUND } from "@angular/core/src/di/injector";
+declare var $: any;
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'sugarcare90';
+  title = "sugarcare90";
+  constructor() {}
+
+  submitform(form: NgForm) { 
+    console.log(form.value);
+    form.reset();
+    $('#exampleModalCenter').modal('hide')
+  }
+
+  openModal(form: NgForm) {
+    let that=this;
+    $("#exampleModalCenter")
+      .modal({
+        keyboard: false,
+        toggle: true
+      })
+      .on("hidden.bs.modal", function(e) {
+        // console.log("dsdsd");
+        that.submitform(form);
+      })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -12,7 +12,8 @@ import { HeaderComponent } from './components/header/header.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
